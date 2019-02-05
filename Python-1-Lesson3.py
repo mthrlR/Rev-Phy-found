f48f7dd3331692e60a9675abf2216f3051ca0606
mthrlR <46788721+mthrlR@users.noreply.github.com>
2019-02-05 13:55:40 +0700

    some changes

--- Python-1-Lesson3.py
+++ Python-1-Lesson3.py
@@ -1,4 +1,22 @@
-#  Task one
+#  Task one easy
+"""
+name = input("Write down ur name ")
+age = input("How old are u ")
+citizenship = input("In what town do u leave ")
+def f(name, age, citizenship):
+    return name + age + citizenship
+print(name + ', ' + age + 'год(а)' + ' ,проживает в городе ' + citizenship)
+"""
+
+#  Task two easy
+#print(zip(lambda x,y,z: max(x, y, z)))
+#    return max(num(x,y,z))
+x = int(input('Введите число Х'))
+y = int(input('Введите число Y'))
+z = int(input('Введите число Z'))
+#print(max(x,y,z))
+def num()
+
 
 
 
