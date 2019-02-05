6a093aa91cd4a101c69c33fb92859918a4d2a6d2
mthrlR <46788721+mthrlR@users.noreply.github.com>
2019-02-05 18:14:12 +0700

    task 2 normal

--- Python-1-Lesson-4.py
+++ Python-1-Lesson-4.py
@@ -23,17 +23,60 @@ print(third)
 """
 #Task three
 """
-list_a = range(-10, 30)
-list_b = []
-for num in list_a:
-    if 0 < num:
-        if num % 3 == 0:
-            if num % 4 > 0:
-                list_b.append(num)
-print(list_b)
+# list_a = range(-10, 30)
+# list_b = []
+# for num in list_a:
+#     if 0 < num:
+#         if num % 3 == 0:
+#             if num % 4 > 0:
+#                 list_b.append(num)
+# print(list_b)
+list_a = [i for i in range(-10, 30) if i > 0 and i % 3 == 0 and i % 4 > 0]
+print(list_a)
 """
 
-#NORMAL BLOCK
-name = input('Ur name?')
-surname = input('Ur surname?')
-email = input('Ur email?')
+# #NORMAL BLOCK
+# name = input('Ur name?')
+# surname = input('Ur surname?')
+# email = input('Ur email?')
+
+#Task two
+
+import re
+pushkin = """
+Мороз и солнце; день чудесный!
+Еще ты дремлешь, друг прелестный —
+Пора, красавица, проснись:
+Открой сомкнуты негой взоры
+Навстречу северной Авроры,
+Звездою севера явись!
+Вечор, ты помнишь, вьюга злилась,
+На мутном небе мгла носилась;
+Луна, как бледное пятно,
+Сквозь тучи мрачные желтела,
+И ты печальная сидела —
+А нынче... погляди в окно:
+Под голубыми небесами
+Великолепными коврами,
+Блестя на солнце, снег лежит;
+Прозрачный лес один чернеет,
+И ель сквозь иней зеленеет,
+И речка подо льдом блестит.
+Вся комната янтарным блеском
+Озарена. Веселым треском
+Трещит затопленная печь.
+Приятно думать у лежанки.
+Но знаешь: не велеть ли в санки
+Кобылку бурую запречь?
+Скользя по утреннему снегу,
+Друг милый, предадимся бегу
+Нетерпеливого коня
+И навестим поля пустые,
+Леса, недавно столь густые,
+И берег, милый для меня."""
+pattern = '\.{3}'
+result = re.search(pattern, pushkin)
+if result:
+    print("Patter match found")
+else:
+    print("Pattern doesn't match")
