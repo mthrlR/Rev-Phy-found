0d8745b1e0437e6ff1e3766d8661745a2d190166
mthrlR <46788721+mthrlR@users.noreply.github.com>
2019-02-07 22:50:38 +0700

    easy clear

--- Python-1-Lesson-5.py
+++ Python-1-Lesson-5.py
@@ -2,25 +2,25 @@ import os
 import sys
 import shutil
 
-#print('sys.argv = ', sys.argv)
-#print('sys.path = ', sys.path)
-#print('sys.executable =', sys.executable)
-#print('sys.modules = ', sys.modules)
-#print('sys.platform = ', sys.platform)
+# print('sys.argv = ', sys.argv)
+# print('sys.path = ', sys.path)
+# print('sys.executable =', sys.executable)
+# print('sys.modules = ', sys.modules)
+# print('sys.platform = ', sys.platform)
 
 # for path in sys.path:
 #     print(path)
-#print(os.getcwd())
-#print(os.listdir())
-#print(1000)
+# print(os.getcwd())
+# print(os.listdir())
+# print(1000)
 
-#Easy part
-#Task one
+# Easy part
+# Task one
 """
 # print("Текущая рабочая директория %s" % path)
 # print("{}{}".format('Текущая рабочая директория ', os.getcwd()))
 # 
-# print("{}{}".format('Создана новая директория', os.mkdir('test'))) \\ почему формат не отображает название директории?
+# print("{}{}".format('Создана новая директория', os.mkdir('test'))) ## почему формат не отображает название директории?
 # print("{}{}".format('Удалена директория', os.rmdir('test')))
 # print("{}{}".format('Созданы новае директории', os.makedirs('1/2/3/4/5/6/7/8')))
 # print("{}{}".format('Удалена директория', shutil.rmtree('1')))
@@ -34,15 +34,57 @@ print("{}{}".format('Текущая рабочая директория ', os.ge
 shutil.rmtree('test')
 """
 
-#Task two
+# Task two
 """
-print("{}{}".format('Текущая рабочая директория ', os.getcwd()))
 os.mkdir('test')
 os.chdir('test')
-list = ['jan', 'feb', 'march', 'aprl', 'may', 'jun', 'jul', 'aug']
-for month in list:
+clndr = ['jan', 'feb', 'march', 'aprl', 'may', 'jun', 'jul', 'aug']
+for month in clndr:
     os.makedirs(month)
-os.chdir('C:/Users/reverdatto/PycharmProjects/Rev-Phy-found')
-print(os.listdir('test'))
-#shutil.rmtree('test')
+print("{}{}".format('Текущая рабочая директория ', os.getcwd()))
+os.chdir('C:/Users/Reverdatto/pycharmProjects/Rev-Phy-found')
+print("{}{}{}{}".format('Текущая рабочая директория: ', os.getcwd(), '. Список директорий: ', os.listdir('test')))
+#print(os.listdir('test'))
+# shutil.rmtree('test')
 """
+# Task three
+"""
+shutil.copy2('Python-1-Lesson-5.py', 'new_py.py')
+"""
+
+# Normal part
+# Task one
+print('sys.argv = ', sys.argv)
+def print_help():
+    print("help - получение справки")
+    print("mkdir <dir_name> - создание директории")
+    print("ping - тестовый ключ")
+def make_dir():
+    if not dir_name:
+        print("Необходимо указать имя директории вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print('директория {} создана'.format(dir_name))
+    except FileExistsError:
+        print('директория {} уже существует'.format(dir_name))
+
+do = {
+    "help": print_help,
+    "mkdir": make_dir,
+}
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
