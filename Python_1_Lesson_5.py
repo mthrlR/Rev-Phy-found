c32f7e035143b2c03263f1edd98b030d9d0c60ce
mthrlR <46788721+mthrlR@users.noreply.github.com>
2019-02-08 09:54:46 +0700

    final

--- Python_1_Lesson_5.py
+++ Python_1_Lesson_5.py
@@ -49,41 +49,6 @@ print("{}{}{}{}".format('Текущая рабочая директория: ', 
 """
 # Task three
 """
-shutil.copy2('Python-1-Lesson-5.py', 'new_py.py')
+shutil.copy2('Python_1_Lesson_5.py', 'new_py.py')
 """
 
-# DZ
-print('sys.argv = ', sys.argv)
-def print_help():
-    print("help - получение справки")
-    print("mkdir <dir_name> - создание директории")
-    print("ping - тестовый ключ")
-def make_dir():
-    if not dir_name:
-        print("Необходимо указать имя директории вторым параметром")
-        return
-    dir_path = os.path.join(os.getcwd(), dir_name)
-    try:
-        os.mkdir(dir_path)
-        print('директория {} создана'.format(dir_name))
-    except FileExistsError:
-        print('директория {} уже существует'.format(dir_name))
-
-do = {
-    "help": print_help,
-    "mkdir": make_dir,
-}
-try:
-    dir_name = sys.argv[2]
-except IndexError:
-    dir_name = None
-try:
-    key = sys.argv[1]
-except IndexError:
-    key = None
-if key:
-    if do.get(key):
-        do[key]()
-    else:
-        print("Задан неверный ключ")
-        print("Укажите ключ help для получения справки")
